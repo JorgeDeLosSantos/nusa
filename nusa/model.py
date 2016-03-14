c8fe3d8da10ed7c7232cafe3c9a6e1ba62987bc0
Jorge De Los Santos <delossantosmfq@gmail.com>
2016-03-13 23:46:36 -0600

    TrussModel Enhancement

--- nusa/model.py
+++ nusa/model.py
@@ -541,7 +541,157 @@ class TrussModel(Model):
         nnodes=self.getNumberOfNodes(),
         displacements=self.getDisplacements(),
         nodalforces=self.getNodalForces(),
-        elementforces=self.getElementForces(),
+        elementforces="",
+        version=__version__)
+        return _str
+        
+    def fileReport(self,filename):
+        """
+        Write report to file
+        """
+        out_str = self.CLIReport()
+        f = open(filename,"w")
+        f.write(out_str)
+        f.close()
+
+
+class TrussModel2(Model):
+    """
+    Model for finite element analysis
+    """
+    def __init__(self,name="Truss Model 01"):
+        Model.__init__(self,name=name,mtype="truss")
+        self.F = [] # Forces
+        self.U = [] # Displacements
+        self.dof = 2 # 2 DOF for truss element
+        
+    def buildForcesVector(self):
+        for node in self.nodes.values():
+            fx, fy = node.getForces()
+            self.F.append(fx)
+            self.F.append(fy)
+        self.F = np.array(self.F)
+        
+    def buildGlobalMatrix(self):
+        msz = (self.dof)*self.getNumberOfNodes()
+        self.KG = np.zeros((msz,msz))
+        for element in self.elements.values():
+            ku = element.getElementStiffness()
+            n1,n2 = element.getNodes()
+            self.KG[2*n1.label, 2*n1.label] += ku[0,0]
+            self.KG[2*n1.label, 2*n1.label+1] += ku[0,1]
+            self.KG[2*n1.label, 2*n2.label] += ku[0,2]
+            self.KG[2*n1.label, 2*n2.label+1] += ku[0,3]
+            
+            self.KG[2*n1.label+1, 2*n1.label] += ku[1,0]
+            self.KG[2*n1.label+1, 2*n1.label+1] += ku[1,1]
+            self.KG[2*n1.label+1, 2*n2.label] += ku[1,2]
+            self.KG[2*n1.label+1, 2*n2.label+1] += ku[1,3]
+            
+            self.KG[2*n2.label, 2*n1.label] += ku[2,0]
+            self.KG[2*n2.label, 2*n1.label+1] += ku[2,1]
+            self.KG[2*n2.label, 2*n2.label] += ku[2,2]
+            self.KG[2*n2.label, 2*n2.label+1] += ku[2,3]
+            
+            self.KG[2*n2.label+1, 2*n1.label] += ku[3,0]
+            self.KG[2*n2.label+1, 2*n1.label+1] += ku[3,1]
+            self.KG[2*n2.label+1, 2*n2.label] += ku[3,2]
+            self.KG[2*n2.label+1, 2*n2.label+1] += ku[3,3]
+            #~ print element.label, 2*n1.label, 2*n1.label+1, 2*n2.label, 2*n2.label+1
+            
+        #~ self.buildForcesVector()
+        #~ self.buildDisplacementsVector()
+        
+    def buildDisplacementsVector(self):
+        for node in self.nodes.values():
+            ux, uy = node.getDisplacements()
+            self.U.append(ux)
+            self.U.append(uy)
+        self.U = np.array(self.U)
+    
+    def addForce(self,node,force):
+        node.setForces(fx=force[0], fy=force[1])
+        
+    def addConstraint(self,node,**constraint):
+        cs = constraint
+        if cs.has_key('ux') and cs.has_key("uy"):
+            ux = cs.get('ux')
+            uy = cs.get('uy')
+            node.setDisplacements(ux=ux, uy=uy)
+        elif cs.has_key('ux'):
+            ux = cs.get('ux')
+            node.setDisplacements(ux=ux)
+        elif cs.has_key('uy'):
+            uy = cs.get('uy')
+            node.setDisplacements(uy=uy)
+        
+    def solve(self):
+        self.buildDisplacementsVector()
+        self.buildForcesVector()
+        knw = [pos for pos,value in enumerate(self.U) if not np.isnan(value)]
+        unknw = [pos for pos,value in enumerate(self.U) if np.isnan(value)]
+        self.K2S = np.delete(np.delete(self.KG,knw,0),knw,1)
+        self.F2S = np.delete(self.F,knw,0)
+        # For displacements
+        self.solved_u = la.solve(self.K2S,self.F2S)
+        for k,idx in enumerate(unknw):
+            nd = idx/2
+            self.U[idx] = self.solved_u[k]
+            if (-1)**idx > 0:
+                self.nodes[nd].ux = self.solved_u[k]
+            else:
+                self.nodes[nd].uy = self.solved_u[k]
+        #~ # For nodal forces/reactions
+        self.NF = self.F.copy()
+        nf_calc = np.dot(self.KG, self.U)
+        for nd in self.nodes.keys():
+            self.NF[2*nd] = nf_calc[2*nd]
+            self.NF[2*nd+1] = nf_calc[2*nd+1]
+        print self.NF
+            
+    def getDisplacements(self):
+        _str = ""
+        for lbl,u in self.U.items():
+            _str += (str(lbl+1) + "\t|\t" + str(u) + "\n")
+        return _str
+                
+    def getNodalForces(self):
+        _str = ""
+        for lbl,nf in self.NF.items():
+            _str += (str(lbl+1) + "\t|\t" + str(nf) + "\n")
+        return _str
+    
+    def getElementForces(self):
+        _str = ""
+        for lbl,ef in self.EF.items():
+            _str += (str(lbl+1) + "\t|\t" + str(ef) + "\n")
+        return _str
+        
+    def report(self,out="cli",filename="report.txt"):
+        """
+        Write a mini-report for results.
+        
+        If out is 'cli' then show information in python console.
+        """
+        if out=="cli":
+            print(self.CLIReport())
+        elif out=="gui":
+            pass
+        elif out=="file":
+            self.fileReport(filename)
+        
+    def CLIReport(self):
+        """
+        String for Spring Model information
+        """
+        _str = tmp.MINI_REPORT_TEMPLATE.format(
+        model=self.name,
+        mtype=self.mtype,
+        nelements=self.getNumberOfElements(),
+        nnodes=self.getNumberOfNodes(),
+        displacements=self.getDisplacements(),
+        nodalforces=self.getNodalForces(),
+        elementforces="",
         version=__version__)
         return _str
         
