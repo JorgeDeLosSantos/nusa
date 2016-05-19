c9920ff54e5403bd543434316b77fa7f376a997c
Pedro Jorge De Los Santos <delossantosmfq@gmail.com>
2016-05-19 11:52:01 -0500

    Minor changes

--- nusa/model.py
+++ nusa/model.py
@@ -14,17 +14,19 @@ from _info import __version__,__organization__
 
 class SpringModel(Model):
     """
-    Model for finite element analysis
+    Spring Model for finite element analysis
     """
     def __init__(self,name="Spring Model 01"):
         Model.__init__(self,name=name,mtype="spring")
-        self.F = {}
-        self.U = {}
+        self.F = []
+        self.U = []
         self.dof = 1 # 1 DOF for spring element
         
     def buildForcesVector(self):
         for node in self.nodes.values():
-            self.F[node.label] = 0
+            fx, _ = node.getForces()
+            self.F.append(fx)
+        self.F = np.array(self.F)
         
     def buildGlobalMatrix(self):
         msz = (self.dof)*self.getNumberOfNodes()
@@ -36,91 +38,50 @@ class SpringModel(Model):
             self.KG[n1.label, n2.label] += ku[0,1]
             self.KG[n2.label, n1.label] += ku[1,0]
             self.KG[n2.label, n2.label] += ku[1,1]
-        self.buildForcesVector()
-        self.buildDisplacementsVector()
         
     def buildDisplacementsVector(self):
-        msz = (self.dof)*self.getNumberOfNodes()
         for node in self.nodes.values():
-            self.U[node.label] = np.nan
+            ux, _ = node.getDisplacements()
+            self.U.append(ux)
+        self.U = np.array(self.U)
         
     def addForce(self,node,force):
-        self.F[node.label] = force[0]
+        node.setForces(fx=force[0])
         
     def addConstraint(self,node,**constraint):
-        label = node.label
         if constraint.has_key('ux'):
-            val = constraint.get('ux')
-            node.setDisplacements(ux=val)
-            self.U[label] = val
+            ux = constraint.get('ux')
+            node.setDisplacements(ux=ux)
         
     def solve(self):
-        knw = [lbl for lbl in self.U.keys() if not self.U[lbl] is np.nan]
-        unknw = [lbl for lbl in self.U.keys() if self.U[lbl] is np.nan]
+        # Build forces and displacements vectors.
+        self.buildDisplacementsVector()
+        self.buildForcesVector()
+        # known and unknown values
+        knw = [pos for pos,val in enumerate(self.U) if not np.isnan(val)]
+        unknw = [pos for pos,val in enumerate(self.U) if np.isnan(val)]
+        # Matrices to solve
         self.K2S = np.delete(np.delete(self.KG,knw,0),knw,1)
-        self.F2S = np.delete(self.F.values(),knw,0)
+        self.F2S = np.delete(self.F,knw,0)
         # For displacements
-        self.solved_u = la.solve(self.K2S,self.F2S)
-        self.U.update(zip(unknw,self.solved_u))
+        self.solved_u = la.solve(self.K2S, self.F2S)
+        # Updating U (displacements vector)
+        for k,idx in enumerate(unknw):
+            self.U[idx] = self.solved_u[k]
+            self.nodes[idx].ux = self.solved_u[k]
         # For nodal forces/reactions
         self.NF = self.F.copy()
-        nf_calc = np.dot(self.KG,self.U.values())
-        self.NF.update(zip(self.NF.keys(),nf_calc))
+        nf_calc = np.dot(self.KG,self.U)
+        for k,_ in enumerate(self.getNodes()):
+            self.NF[k] = nf_calc[k]
         # For element forces
-        self.EF = self.elements.copy()
-        for elm in self.elements.values():
+        for elm in self.getElements():
             _ke = elm.getElementStiffness()
             na,nb = elm.getNodes()
             una = self.U[na.label]
             unb = self.U[nb.label]
             _uv = np.array([una,unb])
-            self.EF.update([(elm.label,np.dot(_ke,_uv)),])
-            
-    def getDisplacements(self):
-        _str = ""
-        for lbl,u in self.U.items():
-            _str += (str(lbl+1) + "\t|\t" + str(u) + "\n")
-        return _str
-                
-    def getNodalForces(self):
-        _str = ""
-        for lbl,nf in self.NF.items():
-            _str += (str(lbl+1) + "\t|\t" + str(nf) + "\n")
-        return _str
-    
-    def getElementForces(self):
-        _str = ""
-        for lbl,ef in self.EF.items():
-            _str += (str(lbl+1) + "\t|\t" + str(ef) + "\n")
-        return _str
-        
-    def report(self,out="cli"):
-        """
-        Write a mini-report for results.
-        
-        If out is 'cli' then show information in python console.
-        """
-        if out=="cli":
-            print(self.CLIReport())
-        elif out=="gui":
-            pass
-        elif out=="file":
-            pass
-        
-    def CLIReport(self):
-        """
-        String for Spring Model information
-        """
-        _str = tmp.MINI_REPORT_TEMPLATE.format(
-        model=self.name,
-        mtype=self.mtype,
-        nelements=self.getNumberOfElements(),
-        nnodes=self.getNumberOfNodes(),
-        displacements=self.getDisplacements(),
-        nodalforces=self.getNodalForces(),
-        elementforces=self.getElementForces(),
-        version=__version__)
-        return _str
+            elm.fx = np.dot(_ke, _uv) # Set element force
         
 
 class BarModel(Model):
@@ -186,61 +147,6 @@ class BarModel(Model):
             unb = self.U[nb.label]
             _uv = np.array([una,unb])
             self.EF.update([(elm.label,np.dot(_ke,_uv)),])
-            
-    def getDisplacements(self):
-        _str = ""
-        for lbl,u in self.U.items():
-            _str += (str(lbl+1) + "\t|\t" + str(u) + "\n")
-        return _str
-                
-    def getNodalForces(self):
-        _str = ""
-        for lbl,nf in self.NF.items():
-            _str += (str(lbl+1) + "\t|\t" + str(nf) + "\n")
-        return _str
-    
-    def getElementForces(self):
-        _str = ""
-        for lbl,ef in self.EF.items():
-            _str += (str(lbl+1) + "\t|\t" + str(ef) + "\n")
-        return _str
-        
-    def report(self,out="cli",filename="report.txt"):
-        """
-        Write a mini-report for results.
-        
-        If out is 'cli' then show information in python console.
-        """
-        if out=="cli":
-            print(self.CLIReport())
-        elif out=="gui":
-            pass
-        elif out=="file":
-            self.fileReport(filename)
-        
-    def CLIReport(self):
-        """
-        String for Spring Model information
-        """
-        _str = tmp.MINI_REPORT_TEMPLATE.format(
-        model=self.name,
-        mtype=self.mtype,
-        nelements=self.getNumberOfElements(),
-        nnodes=self.getNumberOfNodes(),
-        displacements=self.getDisplacements(),
-        nodalforces=self.getNodalForces(),
-        elementforces=self.getElementForces(),
-        version=__version__)
-        return _str
-        
-    def fileReport(self,filename):
-        """
-        Write report to file
-        """
-        out_str = self.CLIReport()
-        f = open(filename,"w")
-        f.write(out_str)
-        f.close()
 
 
 class BeamModel(Model):
@@ -344,218 +250,9 @@ class BeamModel(Model):
         node = idx/2
         var = opts[0] if ((-1)**idx)==1 else opts[1]
         return node,var
-            
-    def getDisplacements(self):
-        _str = ""
-        for lbl,u in self.U.items():
-            _str += (str(lbl+1) + "\t|\t" + str(u) + "\n")
-        return _str
-                
-    def getNodalForces(self):
-        _str = ""
-        for lbl,nf in self.NF.items():
-            _str += (str(lbl+1) + "\t|\t" + str(nf) + "\n")
-        return _str
-    
-    def getElementForces(self):
-        _str = ""
-        for lbl,ef in self.EF.items():
-            _str += (str(lbl+1) + "\t|\t" + str(ef) + "\n")
-        return _str
-        
-    def report(self,out="cli",filename="report.txt"):
-        """
-        Write a mini-report for results.
-        
-        If out is 'cli' then show information in python console.
-        """
-        if out=="cli":
-            print(self.CLIReport())
-        elif out=="gui":
-            pass
-        elif out=="file":
-            self.fileReport(filename)
-        
-    def CLIReport(self):
-        """
-        String for Spring Model information
-        """
-        _str = tmp.MINI_REPORT_TEMPLATE.format(
-        model=self.name,
-        mtype=self.mtype,
-        nelements=self.getNumberOfElements(),
-        nnodes=self.getNumberOfNodes(),
-        displacements=self.getDisplacements(),
-        nodalforces=self.getNodalForces(),
-        elementforces=self.getElementForces(),
-        version=__version__)
-        return _str
-        
-    def fileReport(self,filename):
-        """
-        Write report to file
-        """
-        out_str = self.CLIReport()
-        f = open(filename,"w")
-        f.write(out_str)
-        f.close()
         
 
 class TrussModel(Model):
-    """
-    Model for finite element analysis
-    """
-    def __init__(self,name="Truss Model 01"):
-        Model.__init__(self,name=name,mtype="truss")
-        self.F = {} # Forces
-        self.U = {} # Displacements
-        self.dof = 2 # 2 DOF for truss element
-        
-    def buildForcesVector(self):
-        for node in self.nodes.values():
-            self.F[node.label] = {"fx":0, "fy":0} # (fy, m)
-        
-    def buildGlobalMatrix(self):
-        msz = (self.dof)*self.getNumberOfNodes()
-        self.KG = np.zeros((msz,msz))
-        for element in self.elements.values():
-            ku = element.getElementStiffness()
-            n1,n2 = element.getNodes()
-            self.KG[2*n1.label, 2*n1.label] += ku[0,0]
-            self.KG[2*n1.label, 2*n1.label+1] += ku[0,1]
-            self.KG[2*n1.label, 2*n2.label] += ku[0,2]
-            self.KG[2*n1.label, 2*n2.label+1] += ku[0,3]
-            
-            self.KG[2*n1.label+1, 2*n1.label] += ku[1,0]
-            self.KG[2*n1.label+1, 2*n1.label+1] += ku[1,1]
-            self.KG[2*n1.label+1, 2*n2.label] += ku[1,2]
-            self.KG[2*n1.label+1, 2*n2.label+1] += ku[1,3]
-            
-            self.KG[2*n2.label, 2*n1.label] += ku[2,0]
-            self.KG[2*n2.label, 2*n1.label+1] += ku[2,1]
-            self.KG[2*n2.label, 2*n2.label] += ku[2,2]
-            self.KG[2*n2.label, 2*n2.label+1] += ku[2,3]
-            
-            self.KG[2*n2.label+1, 2*n1.label] += ku[3,0]
-            self.KG[2*n2.label+1, 2*n1.label+1] += ku[3,1]
-            self.KG[2*n2.label+1, 2*n2.label] += ku[3,2]
-            self.KG[2*n2.label+1, 2*n2.label+1] += ku[3,3]
-            #~ print element.label, 2*n1.label, 2*n1.label+1, 2*n2.label, 2*n2.label+1
-            
-        self.buildForcesVector()
-        self.buildDisplacementsVector()
-        
-    def buildDisplacementsVector(self):
-        for node in self.nodes.values():
-            self.U[node.label] = {"ux":np.nan, "uy":np.nan} # (uy, r)
-    
-    def addForce(self,node,force):
-        self.F[node.label]["fx"] = force[0]
-        self.F[node.label]["fy"] = force[1]
-        
-    def addConstraint(self,node,**constraint):
-        cs = constraint
-        if cs.has_key('ux') and cs.has_key("uy"):
-            ux = cs.get('ux')
-            uy = cs.get('uy')
-            node.setDisplacements(ux=ux, uy=uy)
-            print("Fijado")
-            self.U[node.label]["ux"] = ux
-            self.U[node.label]["uy"] = uy
-        elif cs.has_key('ux'):
-            ux = cs.get('ux')
-            node.setDisplacements(ux=ux)
-            print("Soporte simple x")
-            self.U[node.label]["ux"] = ux
-        elif cs.has_key('uy'):
-            uy = cs.get('uy')
-            node.setDisplacements(uy=uy)
-            print("Soporte simple y")
-            self.U[node.label]["uy"] = uy
-        
-    def solve(self):
-        self.VU = [node[key] for node in self.U.values() for key in ("ux","uy")]
-        self.VF = [node[key] for node in self.F.values() for key in ("fx","fy")]
-        knw = [pos for pos,value in enumerate(self.VU) if not value is np.nan]
-        unknw = [pos for pos,value in enumerate(self.VU) if value is np.nan]
-        self.K2S = np.delete(np.delete(self.KG,knw,0),knw,1)
-        self.F2S = np.delete(self.VF,knw,0)
-        # For displacements
-        self.solved_u = la.solve(self.K2S,self.F2S)
-        for k,ic in enumerate(unknw):
-            nd, var = self.index2key(ic)
-            self.U[nd][var] = self.solved_u[k]
-        # For nodal forces/reactions
-        self.NF = self.F.copy()
-        self.VU = [node[key] for node in self.U.values() for key in ("ux","uy")]
-        nf_calc = np.dot(self.KG, self.VU)
-        for k,ic in enumerate(range(2*self.getNumberOfNodes())):
-            nd, var = self.index2key(ic, ("fx","fy"))
-            self.NF[nd][var] = nf_calc[k]
-        
-            
-    def index2key(self,idx,opts=("ux","uy")):
-        node = idx/2
-        var = opts[0] if ((-1)**idx)==1 else opts[1]
-        return node,var
-            
-    def getDisplacements(self):
-        _str = ""
-        for lbl,u in self.U.items():
-            _str += (str(lbl+1) + "\t|\t" + str(u) + "\n")
-        return _str
-                
-    def getNodalForces(self):
-        _str = ""
-        for lbl,nf in self.NF.items():
-            _str += (str(lbl+1) + "\t|\t" + str(nf) + "\n")
-        return _str
-    
-    def getElementForces(self):
-        _str = ""
-        for lbl,ef in self.EF.items():
-            _str += (str(lbl+1) + "\t|\t" + str(ef) + "\n")
-        return _str
-        
-    def report(self,out="cli",filename="report.txt"):
-        """
-        Write a mini-report for results.
-        
-        If out is 'cli' then show information in python console.
-        """
-        if out=="cli":
-            print(self.CLIReport())
-        elif out=="gui":
-            pass
-        elif out=="file":
-            self.fileReport(filename)
-        
-    def CLIReport(self):
-        """
-        String for Spring Model information
-        """
-        _str = tmp.MINI_REPORT_TEMPLATE.format(
-        model=self.name,
-        mtype=self.mtype,
-        nelements=self.getNumberOfElements(),
-        nnodes=self.getNumberOfNodes(),
-        displacements=self.getDisplacements(),
-        nodalforces=self.getNodalForces(),
-        elementforces="",
-        version=__version__)
-        return _str
-        
-    def fileReport(self,filename):
-        """
-        Write report to file
-        """
-        out_str = self.CLIReport()
-        f = open(filename,"w")
-        f.write(out_str)
-        f.close()
-
-
-class TrussModel2(Model):
     """
     Model for finite element analysis
     """
@@ -597,7 +294,6 @@ class TrussModel2(Model):
             self.KG[2*n2.label+1, 2*n1.label+1] += ku[3,1]
             self.KG[2*n2.label+1, 2*n2.label] += ku[3,2]
             self.KG[2*n2.label+1, 2*n2.label+1] += ku[3,3]
-            #~ print element.label, 2*n1.label, 2*n1.label+1, 2*n2.label, 2*n2.label+1
             
         #~ self.buildForcesVector()
         #~ self.buildDisplacementsVector()
@@ -648,62 +344,6 @@ class TrussModel2(Model):
             self.NF[2*nd] = nf_calc[2*nd]
             self.NF[2*nd+1] = nf_calc[2*nd+1]
         print self.NF
-            
-    def getDisplacements(self):
-        _str = ""
-        for lbl,u in self.U.items():
-            _str += (str(lbl+1) + "\t|\t" + str(u) + "\n")
-        return _str
-                
-    def getNodalForces(self):
-        _str = ""
-        for lbl,nf in self.NF.items():
-            _str += (str(lbl+1) + "\t|\t" + str(nf) + "\n")
-        return _str
-    
-    def getElementForces(self):
-        _str = ""
-        for lbl,ef in self.EF.items():
-            _str += (str(lbl+1) + "\t|\t" + str(ef) + "\n")
-        return _str
-        
-    def report(self,out="cli",filename="report.txt"):
-        """
-        Write a mini-report for results.
-        
-        If out is 'cli' then show information in python console.
-        """
-        if out=="cli":
-            print(self.CLIReport())
-        elif out=="gui":
-            pass
-        elif out=="file":
-            self.fileReport(filename)
-        
-    def CLIReport(self):
-        """
-        String for Spring Model information
-        """
-        _str = tmp.MINI_REPORT_TEMPLATE.format(
-        model=self.name,
-        mtype=self.mtype,
-        nelements=self.getNumberOfElements(),
-        nnodes=self.getNumberOfNodes(),
-        displacements=self.getDisplacements(),
-        nodalforces=self.getNodalForces(),
-        elementforces="",
-        version=__version__)
-        return _str
-        
-    def fileReport(self,filename):
-        """
-        Write report to file
-        """
-        out_str = self.CLIReport()
-        f = open(filename,"w")
-        f.write(out_str)
-        f.close()
-
 
 
 if __name__=='__main__':
