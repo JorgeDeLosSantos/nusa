f9e1cdc3b9786630b6cdf38e67ad7ae9d9ee7330
Pedro Jorge De Los Santos <delossantosmfq@gmail.com>
2016-09-23 19:36:11 -0500

    Deleting LATEX version of nusa-theory

--- docs/nusa-theory/latex-version/README.md
+++
@@ -1,7 +0,0 @@
-### Tesis de maestría
-
-* Tema: **Simulación por elemento finito del proceso de estampado de un tubo de acero SAE 1018**
-* Autor: Pedro Jorge De Los Santos
-
-
-
--- docs/nusa-theory/latex-version/toPDF.bat
+++
@@ -1,7 +0,0 @@
-@ECHO OFF
-pdflatex -interaction=nonstopmode main.tex
-bibtex main.aux
-pdflatex -interaction=nonstopmode main.tex
-pdflatex -interaction=nonstopmode main.tex
-main.pdf
-exit
