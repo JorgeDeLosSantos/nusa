3c3656fa266f0f4682ad4ba3468996823b35cac2
Pedro Jorge De Los Santos <delossantosmfq@gmail.com>
2016-11-13 19:30:30 -0600

    Actualizando...

--- nusa/lib.py
+++ nusa/lib.py
@@ -9,9 +9,31 @@
 Defining some common materials.
 """
 
-import misc
+class Material(object):
+    def __init__(self,name,**kwargs):
+        """
+        
+        Parameters
+        ----------
+        
+        name :    Name of material
+        
+        
+        **kwargs:
+        
+        E       :    Elastic modulus
+        nu      :    Poisson ratio
+        density :    Density
+        
+        """
+        self.name = name
+        self.__dict__ = kwargs # add all properties
+    
+    def __str__(self):
+        return self.name
 
 ## ================== Materials ======================
-STEEL_1018 = misc.Material("1018 Steel",E=205e9,nu=0.3)
-ALUMINIUM_6061 = misc.Material("6061 Aluminium Alloy",E=69e9,nu=0.33)
+
+STEEL_1018 = Material("1018 Steel", E=205e9, nu=0.3)
+ALUMINIUM_6061 = Material("6061 Aluminium Alloy", E=69e9, nu=0.33)
 
