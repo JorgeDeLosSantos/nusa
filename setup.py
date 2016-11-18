7d5acacdfc17541c074f4bc49a07e455feabc275
Pedro Jorge De Los Santos <delossantosmfq@gmail.com>
2016-11-18 13:16:02 -0600

    Actualizando NUSA

--- setup.py
+++ setup.py
@@ -10,11 +10,22 @@ from setuptools import setup
 # Current status: pre-alpha
 
 setup(name='nusa',
-      version='0.1.0.dev0',
-      description='Numerical Structural Analysis with Python',
+      version='0.1.0.dev1',
+      description='Numerical Structural Analysis in Python using the FEM',
       author='Pedro Jorge De Los Santos',
       author_email='delossantosmfq@gmail.com',
       license = "MIT",
+      keywords=["Structural Analysis","FEA","Mechanical Engineering"],
       url='https://github.com/JorgeDeLosSantos/nusa',
       packages=['nusa'],
+      classifiers=[
+      "Development Status :: 2 - Pre-Alpha",
+      "Intended Audience :: Education",
+      "Intended Audience :: Developers",
+      "License :: OSI Approved :: MIT License",
+      "Operating System :: OS Independent",
+      "Programming Language :: Python",
+      "Programming Language :: Python :: 2.7",
+      "Programming Language :: Python :: Implementation :: CPython",
+      ]
       )
