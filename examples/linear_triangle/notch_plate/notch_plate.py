aa2cc1e25fc935c3b062ba1579743f0ae03caca4
JorgeDeLosSantos <delossantosmfq@gmail.com>
2018-08-22 11:01:19 -0500

    Actualizando...

--- examples/linear_triangle/notch_plate/notch_plate.py
+++ examples/linear_triangle/notch_plate/notch_plate.py
@@ -11,7 +11,8 @@ from nusa.mesh import *
 
 m = Modeler()
 a = m.add_poly((0,0),(1,0),(1,1),(0.6,1),(0.5,0.9),(0.4,1),(0,1), esize=0.08)
-nc, ec = m.generate_mesh()
+print(help(m.geom.generate_mesh))
+nc, ec = m.geom.generate_mesh()
 x,y = nc[:,0], nc[:,1]
 
 nodos = []
