c4c3ca11f401b6d67987aef94e4003c056c444d4
Pedro Jorge De Los Santos <delossantosmfq@gmail.com>
2026-09-17 22:17:19 -0600

    Update bar example for property-based model API

--- examples/bar/bar_1.py
+++ examples/bar/bar_1.py
@@ -44,7 +44,7 @@ def test1():
     m1.solve() # Solve model
     
     print("Node | Displacements | Forces")
-    for node in m1.get_nodes():
+    for node in m1.nodes:
         print("{0}\t{1:0.6f}\t{2}".format(node.label, node.ux, node.fx)) 
     
     
