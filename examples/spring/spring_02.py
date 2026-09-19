2cfb4e354979ffc033e7dab8043585d93db69ef3
Pedro Jorge De Los Santos <delossantosmfq@gmail.com>
2026-09-18 22:14:02 -0600

    Update assembly API usage in examples/spring/spring_02.py

--- examples/spring/spring_02.py
+++ examples/spring/spring_02.py
@@ -40,7 +40,7 @@ def test2():
     m2.add_constraint(n5,ux=0.02)
     m2.solve()
 
-    print(f"Global stiffness matrix:\n\n{m2.KG}")
+    print(f"Global stiffness matrix:\n\n{m2.stiffness_matrix}")
     print(f"Displacements of nodes 2,3 and 4: \n\t{n2.ux}\n\t{n3.ux}\n\t{n4.ux}")
     print(f"Nodal forces: \n\t{n1.fx}\n\t{n2.fx}\n\t{n3.fx}\n\t{n4.fx}\n\t{n5.fx} ")
     print(f"Element forces: \n{e1.fx}\n{e2.fx}\n{e3.fx}\n{e4.fx}")
