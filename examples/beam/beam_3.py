b94801078418c4e3a871d7b969ac9ee47a5a954e
Pedro Jorge De Los Santos <delossantosmfq@gmail.com>
2026-09-19 14:52:02 -0600

    Modernize examples/beam/beam_3.py

--- examples/beam/beam_3.py
+++ examples/beam/beam_3.py
@@ -4,10 +4,7 @@
 #  E-mail: delossantosmfq@gmail.com 
 #  License: MIT License
 # ***********************************
-import numpy as np
-from nusa.core import *
-from nusa.model import *
-from nusa.element import *
+from nusa import Beam, BeamModel, Node
 
 def test3():
     """
@@ -39,6 +36,7 @@ def test3():
     m1.solve() # Solve model
     print(m1.stiffness_matrix)
     print([node.uy for node in m1.nodes])
+    return m1
 
 
 if __name__ == '__main__':
