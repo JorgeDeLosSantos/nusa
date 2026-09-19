bc500551d947625fcb172bfca53a822006700857
Pedro Jorge De Los Santos <delossantosmfq@gmail.com>
2026-09-18 22:13:49 -0600

    Update assembly API usage in examples/beam/beam_2.py

--- examples/beam/beam_2.py
+++ examples/beam/beam_2.py
@@ -41,7 +41,7 @@ def test2():
     m1.add_constraint(n1, ux=0, uy=0, ur=0) # fixed 
     m1.add_constraint(n3, ux=0, uy=0, ur=0) # fixed
     m1.solve() # Solve model
-    print(m1.KG)
+    print(m1.stiffness_matrix)
     print(m1.U)
     print(m1.NF)
 
