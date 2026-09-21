5865d0ff8a03c6f3e96ef6e29b48091e38646108
Pedro Jorge De Los Santos <delossantosmfq@gmail.com>
2026-09-21 15:39:13 -0600

    Remove unsupported beam axial constraints

--- examples/beam/beam_3.py
+++ examples/beam/beam_3.py
@@ -31,8 +31,8 @@ def test3():
     for el in (e1,e2): m1.add_element(el)
         
     m1.add_force(n2, (-P,))
-    m1.add_constraint(n1, ux=0, uy=0, ur=0) # fixed 
-    m1.add_constraint(n3, ux=0, uy=0) # fixed
+    m1.add_constraint(n1, uy=0, ur=0) # fixed 
+    m1.add_constraint(n3, uy=0) # fixed
     m1.solve() # Solve model
     print(m1.stiffness_matrix)
     print([node.uy for node in m1.nodes])
